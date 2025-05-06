993fb8377568767347f1ccad1c04109697e88810
Sreerag M S <sreeragmsadanandan@gmail.com>
2025-05-06 22:18:18 +0100

    Added new build config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
+    viteSingleFile(),
   ],
   build: {
     outDir: './dist',
     emptyOutDir: true,
   },
-  base: './',
 });
