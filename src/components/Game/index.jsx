12f120595b365806f9260f8745fcf11a559da0ee
Sreerag M S <sreeragmsadanandan@gmail.com>
2025-05-06 05:18:32 +0100

    Removed controls from the Game

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -2,12 +2,10 @@ import { Content } from 'antd/es/layout/layout';
 import PropTypes from 'prop-types';
 import React from 'react';
 import GridSection from './GridSection';
-import SimulationControls from './SimulationControls';
 
 const Game = ({ setStabilizedModalOpen }) => (
   <Content className="p-3 md:p-6 lg:p-12">
-    <SimulationControls />
-
+    {/* <SimulationControls /> */}
     <div className="pb-4 overflow-auto">
       <GridSection setStabilizedModalOpen={setStabilizedModalOpen} />
     </div>
